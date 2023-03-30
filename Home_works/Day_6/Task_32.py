6a80ed43ea5ad7952b8be65afc33a5dc22b2f8ef
Ivan Tereshchenko <geminimzk@gmail.com>
2023-03-30 14:14:44 +0400

    added lection_4 and HomeWork tasks 30_32

---
+++ Home_works/Day_6/Task_32.py
@@ -0,0 +1,15 @@
+# Задача 32: Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону
+# (т.е. не меньше заданного минимума и не больше заданного максимума)
+from random import randint
+
+total_elems = int(input('Введите количество элементов массива: '))
+
+list_1 = [randint(1, 10) for _ in range(total_elems)]
+print(list_1)
+
+min_range = int(input('Введите минимальный элемент искомого диапазона: '))
+max_range = int(input('Введите максимальный элемент искомого диапазона: '))
+
+for index, elem in enumerate(list_1):
+    if min_range <= elem <= max_range:
+        print(index)
